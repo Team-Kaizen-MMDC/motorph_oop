b27df870d8a2c0cec912cc6770a59ac79b71f69e
Brian Jan Carlos <41847348+brianjancarlos@users.noreply.github.com>
2025-02-18 22:12:25 +0800

    Initial Commit

---
+++ motorph_oop/src/main/java/com/teamkaizen/motorph_oop/Motorph_oop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.teamkaizen.motorph_oop;
+
+/**
+ *
+ * @author brianjancarlos
+ */
+public class Motorph_oop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
