ab3c45e1582d0e38b895d64f9d48c6bb21f63cd0
Brian Jan Carlos <41847348+brianjancarlos@users.noreply.github.com>
2025-03-01 21:50:09 +0800

    format DB connection

--- motorph_oop/src/main/java/services/dbconnection.java
+++ motorph_oop/src/main/java/services/dbconnection.java
@@ -4,10 +4,21 @@
  */
 package services;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author brianjancarlos
  */
 public class dbconnection {
-    
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/motorph_oop";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
